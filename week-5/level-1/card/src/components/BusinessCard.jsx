704cad9db82c59c15f5d9f1cfb4c23f0a815d2cd
aman-4321 <amanclash4321@gmail.com>
2024-04-13 22:31:32 +0530

    completed week 5 solutions

--- week-5/level-1/card/src/components/BusinessCard.jsx
+++ week-5/level-1/card/src/components/BusinessCard.jsx
@@ -3,21 +3,38 @@ export default function BusinessCard() {
     <div className="h-screen w-screen font-sans pl-2 pt-4">
       <div className=" w-1/3 h-1/2 border-gray-200 border-2 rounded-xl">
         <div className="">
-          <p className="pl-5 pt-2 font-medium text-5xl">Aman kumar</p>
+          <p className="pl-6 pt-2 font-medium text-5xl">Lokeshwar</p>
         </div>
         <div>
-          <p className="pt-5 pl-5 text-2xl">Learning Web Development</p>
+          <p className="pt-5 pl-6 text-xl pb-5">
+            A TA in the 100xDevs Cohort 2.0
+          </p>
         </div>
-        <div className="pl-5">
-          <p className="font-bold">Interests</p>
+        <div className="pl-6 pb-5">
+          <p className="font-bold pb-3">Interests</p>
           <p>Ionic</p>
           <p>Open Source</p>
           <p>App dev</p>
         </div>
-        <div className="">
-          <button className="bg-blue-500 py-2 px-2">LinkedIn</button>
-          <button className="bg-blue-500 pr-5">Twitter</button>
-          <button className="bg-blue-500 pr-5">Github</button>
+        <div className="ml-5">
+          <button
+            className="bg-blue-500 py-3 px-3 mr-5 border rounded-lg text-white font-semibold"
+            onClick={() => window.open("https://www.linkedin.com", "_blank")}
+          >
+            LinkedIn
+          </button>
+          <button
+            className="bg-blue-500 py-3 px-3 mr-5 border rounded-lg text-white font-semibold"
+            onClick={() => window.open("https://www.twitter.com", "_blank")}
+          >
+            Twitter
+          </button>
+          <button
+            className="bg-blue-500 py-3 px-3 mr-5 border rounded-lg text-white font-semibold"
+            onClick={() => window.open("https://www.github.com", "_blank")}
+          >
+            Github
+          </button>
         </div>
       </div>
     </div>
